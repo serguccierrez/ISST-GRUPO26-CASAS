0594458539a83fd6ca16ccb5fccf4e35e8edca6a
Sergio Gutierrez Fernandez <sergio.gutierrez@alumnos.upm.es>
2025-04-25 21:17:53 +0200

    email actualizado

--- IoHome/web/src/pages/Contact.jsx
+++ IoHome/web/src/pages/Contact.jsx
@@ -5,7 +5,9 @@ const Contact = () => (
     <div className="text-container">
       <h1>Contáctanos</h1>
       <p style={{ fontSize: "1.15rem" }}>
-        ¿Tienes dudas o necesitas ayuda? Nuestro equipo está aquí para asistirte. Contáctanos a través de nuestro formulario o correo electrónico y te responderemos lo antes posible.
+        ¿Tienes dudas o necesitas ayuda? Nuestro equipo está aquí para
+        asistirte. Contáctanos a través de nuestro formulario o correo
+        electrónico y te responderemos lo antes posible.
       </p>
     </div>
 
@@ -14,39 +16,50 @@ const Contact = () => (
       <div className="text-container">
         <h1>Soporte rápido y confiable</h1>
         <p style={{ fontSize: "1.15rem" }}>
-          Estamos comprometidos a brindarte el mejor servicio. Aprende cómo configurar tus cerraduras inteligentes y aprovecha al máximo nuestras soluciones para un hogar más seguro y conectado.
+          Estamos comprometidos a brindarte el mejor servicio. Aprende cómo
+          configurar tus cerraduras inteligentes y aprovecha al máximo nuestras
+          soluciones para un hogar más seguro y conectado.
         </p>
-        <h2>soporte@iohome.com</h2>
+        <h2>iohome.company@gmail.com</h2>
       </div>
     </section>
 
-    <section className="form-container" style={{ textAlign: "center", fontSize: "2rem", marginBottom: "1rem", overflowY: "hidden", }}>
-      <h2 style={{ textAlign: "center", fontSize: "2rem", marginBottom: "1rem" }}>
+    <section
+      className="form-container"
+      style={{
+        textAlign: "center",
+        fontSize: "2rem",
+        marginBottom: "1rem",
+        overflowY: "hidden",
+      }}
+    >
+      <h2
+        style={{ textAlign: "center", fontSize: "2rem", marginBottom: "1rem" }}
+      >
         Formulario de Contacto
       </h2>
 
       {/* Aquí se reemplaza el formulario de contacto original por el Google Form usando iframe */}
       <iframe
-  src="https://forms.gle/fGFMT6RzrJ25QYzV7"
-  width="100%"
-  height="800"
-  frameBorder="0"
-  marginHeight="0"
-  marginWidth="0"
-  scrolling="yes" // esto ayuda a quitar el scroll en navegadores antiguos
-  title="Formulario de contacto"
-  style={{
-    maxWidth: "800px",
-    borderRadius: "12px",
-    backgroundColor: "#f9f9f9",
-    border: "none",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    overflow: "hidden",
-    
-  }}
->
-  Cargando…
-</iframe>
+        src="https://forms.gle/fGFMT6RzrJ25QYzV7"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        scrolling="yes" // esto ayuda a quitar el scroll en navegadores antiguos
+        title="Formulario de contacto"
+        style={{
+          maxWidth: "800px",
+          borderRadius: "12px",
+          backgroundColor: "#f9f9f9",
+          border: "none",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          overflow: "hidden",
+        }}
+      >
+        Cargando…
+      </iframe>
     </section>
   </main>
 );
