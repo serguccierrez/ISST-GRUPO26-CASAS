8c89fd8f040915924d6a471f0240b54c53ae757a
Sergio Gutierrez Fernandez <sergio.gutierrez@alumnos.upm.es>
2025-05-08 18:36:07 +0200

    error sesion check

--- IoHome/test/selenium.config.js
+++ IoHome/test/selenium.config.js
@@ -1,9 +1,21 @@
-// selenium.config.js
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+// Crea un directorio temporal único para el perfil de Firefox
+const tempProfile = fs.mkdtempSync(path.join(os.tmpdir(), 'firefox-profile-'));
+
 module.exports = {
-    browsers: [
-      {
-        browserName: 'firefox'
+  browsers: [
+    {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: [
+          '--headless',
+          `--profile=${tempProfile}`, // Asigna un perfil temporal único
+          '--no-remote'  // Asegura que no se intente usar sesiones previas
+        ]
       }
-    ]
-  };
-  
+    }
+  ]
+};
