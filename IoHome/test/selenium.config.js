8acdf4defc52b3b31af891dc672163a26644fa19
Sergio Gutierrez Fernandez <sergio.gutierrez@alumnos.upm.es>
2025-05-08 18:42:31 +0200

    standby del workflow por fallos

--- IoHome/test/selenium.config.js
+++ IoHome/test/selenium.config.js
@@ -1,21 +1,21 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+// const os = require('os');
+// const path = require('path');
+// const fs = require('fs');
 
-// Crea un directorio temporal único para el perfil de Firefox
-const tempProfile = fs.mkdtempSync(path.join(os.tmpdir(), 'firefox-profile-'));
+// // Crea un directorio temporal único para el perfil de Firefox
+// const tempProfile = fs.mkdtempSync(path.join(os.tmpdir(), 'firefox-profile-'));
 
-module.exports = {
-  browsers: [
-    {
-      browserName: 'firefox',
-      'moz:firefoxOptions': {
-        args: [
-          '--headless',
-          `--profile=${tempProfile}`, // Asigna un perfil temporal único
-          '--no-remote'  // Asegura que no se intente usar sesiones previas
-        ]
-      }
-    }
-  ]
-};
+// module.exports = {
+//   browsers: [
+//     {
+//       browserName: 'firefox',
+//       'moz:firefoxOptions': {
+//         args: [
+//           '--headless',
+//           `--profile=${tempProfile}`, // Asigna un perfil temporal único
+//           '--no-remote'  // Asegura que no se intente usar sesiones previas
+//         ]
+//       }
+//     }
+//   ]
+// };
